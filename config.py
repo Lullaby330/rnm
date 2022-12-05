d7cd8d3899357a0479fcd3350a5387d5a0c49767
Lullaby330 <69coder@gmail.com>
2022-12-06 03:31:52 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,34 @@
+ import os
 
-class Development(Config):
-  APP_ID = "6216349"
-  API_HASH = "5c7418e9f3df6db931caa7354521c55f"
-  TG_BOT_TOKEN = "5880563885:AAHf0Fi8x_iNDfHmpL0y03KO3sUxnn_ZlE0"
-  AUTH_USERS = [1823080600]
-  CHANNEL1_ID = "-1001854758671"                            
-  CHANNEL1_NAME = "File Rename"
-  CHANNEL2_ID = ""
-  CHANNEL2_NAME = ""
-  CHANNEL3_ID = ""
-  CHANNEL3_NAME = ""
-  CHANNEL4_ID = ""
-  CHANNEL4_NAME = ""
-  CHANNEL5_ID = ""
-  CHANNEL5_NAME = ""
+class Config(object):    
+    # get a token from @BotFather
+    TG_BOT_TOKEN = os.environ.get("5880563885:AAHf0Fi8x_iNDfHmpL0y03KO3sUxnn_ZlE0")
+
+    # Get these values from my.telegram.org
+    APP_ID = int(os.environ.get("6216349"))
+
+    API_HASH = os.environ.get("5c7418e9f3df6db931caa7354521c55f")
+
+    # Array to store users who are authorized to use the bot
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1823080600").split())
+
+    # Channels to forward the formatted video (Optional, Prefix: "-100", Bot should be an admin of the channels)
+    CHANNEL1_ID = os.environ.get("-1001854758671")
+
+    CHANNEL1_NAME = os.environ.get("File Renamer")
+
+    CHANNEL2_ID = os.environ.get("CHANNEL2_ID")
+
+    CHANNEL2_NAME = os.environ.get("CHANNEL2_NAME")
+
+    CHANNEL3_ID = os.environ.get("CHANNEL3_ID")
+
+    CHANNEL3_NAME = os.environ.get("CHANNEL3_NAME")
+
+    CHANNEL4_ID = os.environ.get("CHANNEL4_ID")
+
+    CHANNEL4_NAME = os.environ.get("CHANNEL4_NAME")
+
+    CHANNEL5_ID = os.environ.get("CHANNEL5_ID")
+
+    CHANNEL5_NAME = os.environ.get("CHANNEL5_NAME")
